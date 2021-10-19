fee1f253baf1c95d5f9eb7e0667926a94c14aa14
Rui Flexa <ruiflexa@gmail.com>
2021-10-19 15:34:03 -0300

    csv feito em 2018 pra ler csv de leads

---
+++ Campus.Lib/IO/Configuracao.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Campus.Lib.IO
+{
+    public class Configuracao
+    {
+        
+       
+        
+        public Configuracao()
+        {
+            Delimitador = ";";
+            IgnoreEspacoEmBrancoHeader = true;
+            Map = new List<Mapeamento>();
+            Encondig = Encoding.Default;
+        }
+
+
+        
+       
+        
+        public string Delimitador { get; set; }
+
+        public bool IgnoreEspacoEmBrancoHeader { get; set; }
+        
+        
+        public bool RemoverEspacoEmBrancoCampos { get; set; }
+
+        
+        public Encoding Encondig { get; set; }
+
+        
+        public ICollection<Mapeamento> Map;
+
+    }
+
+
+    
+ 
+    
+    public class Mapeamento
+    {
+        
+
+        
+        private Mapeamento()
+        {
+        }
+
+
+        public Mapeamento(string nomeDaPropriedade, string nomeDaColuna) : this()
+        {
+            NomeDaPropriedade = nomeDaPropriedade;
+            NomeDaColuna = nomeDaColuna;
+        }
+
+        public Mapeamento(string nomeDaPropriedade) : this(nomeDaPropriedade, nomeDaPropriedade)
+        {
+        }
+
+
+        
+        public string NomeDaPropriedade { get; set; }
+        
+
+        
+        public string NomeDaColuna { get; set; }
+        
+  
+        public TipoConversao Converter { get; private set; }
+
+        public void AddConvert(Func<string, dynamic> conversao)
+        {
+            AddConvert(NomeDaColuna, conversao);
+        }
+
+
+        public void AddConvert(string nomeDaColunae, Func<string, dynamic> conversao)
+        {
+            Converter = new TipoConversao()
+            {
+                NomeDaColuna = nomeDaColunae,
+                Conversao = conversao
+            };
+        }
+    }
+
+
+    
+    public class TipoConversao
+    {
+
+        
+        public string NomeDaColuna { get; set; }
+
+        public Func<string, dynamic> Conversao { get; set; }
+    }
+}
---
+++ Campus.Lib/IO/CsvConvert.cs
@@ -0,0 +1,120 @@
+﻿using CsvHelper;
+using CsvHelper.Configuration;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Campus.Lib.IO
+{
+    public class CsvConvert 
+    {
+        CsvReader _csrReader;
+        CsvWriter _csvWrite;
+
+
+        public CsvConvert()
+        {
+        }
+
+
+        private void config<T>(CsvConfiguration csvConfig, Configuracao config)
+        {
+            config = config == null ? new Configuracao() : config;
+            csvConfig.Delimiter = config.Delimitador;
+            csvConfig.IgnoreHeaderWhiteSpace = config.IgnoreEspacoEmBrancoHeader;
+            csvConfig.TrimFields = config.RemoverEspacoEmBrancoCampos;
+            if (config.Map.Count > 0)
+            {
+
+                var customerMap = new CsvHelper.Configuration.DefaultCsvClassMap<T>();
+                var sampleObject = Activator.CreateInstance<T>();
+                foreach (var map in config.Map)
+                {
+
+                    var propertyInfo = sampleObject.GetType().GetProperty(map.NomeDaPropriedade);
+                    var newMap = new CsvPropertyMap(propertyInfo);
+                    newMap.Name(map.NomeDaColuna);
+                    if (map.Converter != null)
+                    {
+                        newMap.ConvertUsing(
+                            row => map.Converter.Conversao(row.GetField<string>(map.Converter.NomeDaColuna))
+                            );
+                    }
+                    customerMap.PropertyMaps.Add(newMap);
+                }
+                csvConfig.RegisterClassMap(customerMap);
+            }
+        }
+
+        public T ReadData<T>(string filePah, Configuracao configuracao = null)
+        {
+            using (StreamReader _sr = new StreamReader(filePah))
+            {
+                return ReadData<T>(_sr, configuracao);
+            }
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="streamReader"></param>
+        /// <param name="delimiter"></param>
+        /// <returns></returns>
+        public T ReadData<T>(StreamReader streamReader, Configuracao configuracao = null)
+        {
+            _csrReader = new CsvReader(streamReader);
+            config<T>(_csrReader.Configuration, configuracao);
+
+
+            return _csrReader.GetRecord<T>();
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="filePah"></param>
+        /// <param name="delimiter"></param>
+        /// <returns></returns>
+        public T[] ReadAllData<T>(string filePah, Configuracao configuracao = null)
+        {
+            var encoding = configuracao == null ? Encoding.Default : configuracao.Encondig;
+            using (StreamReader _sr = new StreamReader(filePah, encoding))
+            {
+                return ReadAllData<T>(_sr, configuracao);
+            }
+        }
+
+        public T[] ReadAllData<T>(StreamReader streamReader, Configuracao configuracao = null)
+        {
+            _csrReader = new CsvReader(streamReader);
+            config<T>(_csrReader.Configuration, configuracao);
+            return _csrReader.GetRecords<T>().ToArray();
+        }
+
+        public void WriteData<T>(StreamWriter streamWriter, T[] dados, Configuracao configuracao = null)
+        {
+            _csvWrite = new CsvWriter(streamWriter);
+            config<T>(_csvWrite.Configuration, configuracao);
+            _csvWrite.WriteHeader<T>();
+            foreach (T item in dados)
+            {
+                _csvWrite.WriteRecord<T>(item);
+            }
+        }
+
+
+        public void WriteData<T>(string filePah, T[] dados, Configuracao configuracao = null)
+        {
+            var encoding = configuracao == null ? Encoding.Default : configuracao.Encondig;
+            using (StreamWriter _sr = new StreamWriter(filePah, true, encoding))
+            {
+                WriteData(_sr, dados, configuracao);
+                _sr.Close();
+            }
+        }
+    }
+}
