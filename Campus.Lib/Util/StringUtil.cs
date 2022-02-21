fe0f1acd79974d1af07803e6ca79ad2b40e6fae6
Rui Flexa <ruiflexa@gmail.com>
2022-02-21 18:23:26 -0300

    Update StringUtil.cs

--- Campus.Lib/Util/StringUtil.cs
+++ Campus.Lib/Util/StringUtil.cs
@@ -26,5 +26,64 @@ namespace Campus.Lib.Util
             return nomeCompleto?.Split(' ').FirstOrDefault();
             
         }
+        
+                public static string RemoverAcentos(string text)
+        {
+            StringBuilder sbReturn = new StringBuilder();
+            var arrayText = text.Normalize(NormalizationForm.FormD).ToCharArray();
+            foreach (char letter in arrayText)
+            {
+                if (CharUnicodeInfo.GetUnicodeCategory(letter) != UnicodeCategory.NonSpacingMark)
+                    sbReturn.Append(letter);
+            }
+
+            return sbReturn.ToString();
+        }
+
+        public static string RemoverCaracteresEspeciais(string text)
+        {
+            text = Regex.Replace(text, @"[^0-9a-zA-ZéúíóáÉÚÍÓÁèùìòàÈÙÌÒÀõãñÕÃÑêûîôâÊÛÎÔÂëÿüïöäËYÜÏÖÄçÇ\s]+?", " ");
+            text = Regex.Replace(text, @"\s+", " ");
+            return text;
+        }
+
+        public static string FormatarDescricao(string valor, int maxTamanhoCampo)
+        {
+            if (!string.IsNullOrWhiteSpace(valor))
+            {
+                var tamanhoCampo = valor.Trim().Length;
+                return valor.Trim().Substring(0, (tamanhoCampo >= maxTamanhoCampo ? maxTamanhoCampo : valor.Trim().Length));
+            }
+
+            return valor;
+        }
+
+        public static string ObterIniciaisNome(string nome)
+        {
+            String[] palavras = nome.TrimEnd().Split(" ");
+            var iniciais = string.Empty;
+            for (int i = 0; i < palavras.Length; i++)
+            {
+                if (palavras[i].Trim() != string.Empty)
+                    iniciais += palavras[i].Trim().Substring(0, 1).ToUpper();
+            }
+            return iniciais;
+        }
+
+        public static string SomenteNumeros(string texto)
+        {
+            return Regex.Replace(texto, @"[^\d]", "");
+        }
+
+        public static string RetornarNumTelefone(string telefone, string parte)
+        {
+            var regexTelefone = Regex.Match(telefone, @"\((\d{2})\)\s?(\d{4,5}\-?\d{4})");
+            return parte switch
+            {
+                "DDD" => SomenteNumeros(regexTelefone.Groups[1].ToString()),
+                "TELEFONE" => SomenteNumeros(regexTelefone.Groups[2].ToString()),
+                _ => SomenteNumeros(telefone)
+            };
+        }
     }
 }
